4ad8949e3de27cefab1013e2d98fc1c7508a1c03
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-06-06 17:27:50 -0700

    GET and POST requests are setup

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,65 @@
 // implement your posts router here
+// this is where I put all the requests once I get it going.
+
+const express = require('express');
+const router =  express.Router();
+const Posts = require('./posts-model')
+
+// GET requests
+router.get('/', (req, res) => {
+    Posts.find()
+      .then(posts => {
+        res.status(200).json(posts);
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Error retrieving the posts' });
+      });
+  });
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Posts.findById(id)
+        .then(posts => {
+            if (!posts) {
+                res.status.status(404).json({message: "The post with the specified ID does not exist"})
+            } else {
+                res.status(200).json(posts);
+            }
+        })
+        .catch(err => {
+            res.status(404).json({message: `The post with the specified ID does not exist`})
+        }) 
+});
+
+// POST requests
+
+router.post('/', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({message: "must include title and contents"})
+    } else {
+        Posts.insert({title, contents})
+        .then(({ id }) => {
+                Posts.findById(id)
+                .then(posts => {
+                    if (!posts) {
+                        res.status.status(400).json({message: "The post with the specified ID does not exist"})
+                    } else {
+                        res.status(201).json(posts);
+                    }
+                })
+                .catch(err => {
+                    res.status(400).json({message: `The post with the specified ID does not exist`})
+                }) 
+            }
+        )
+        .catch(err => {
+            res.status(500).json({ message: 'oh no. not again.'})
+        })
+    }
+})
+
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// require your server and launch it here
+const server = require('./api/server.js');
+
+const port = 9001;
+
+server.listen(port, () => {
+    console.log(`Hello, this is God. This server may exist on port ${port}`)
+});
+
